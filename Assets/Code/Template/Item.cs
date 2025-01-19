338b72a6991c8266bd7cc180a92a40b84e1fdbfb
XomsEdu <144520337+XomsEdu@users.noreply.github.com>
2025-01-20 00:55:42 +0200

    +WIP

---
+++ Assets/Code/Template/Item.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "ScriptableObject/Item")]
+public class Item : ScriptableObject
+{
+    
+    [Header ("Gameplay")]
+    public ItemType type;
+    public Rarity rarity;
+
+    [Header ("UI")]
+    public string title;
+    public Sprite image;
+    public int stacksize = 1;
+
+
+    public enum ItemType {
+        MeleeWeapon,
+        RangeWeapon,
+        Trap,
+        Tool,
+        Armor,
+        Consumable,
+        Ammunition,
+        Building
+    }
+
+    public enum Rarity{
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        Legendary,
+        OneOfAKind
+    }
+}
