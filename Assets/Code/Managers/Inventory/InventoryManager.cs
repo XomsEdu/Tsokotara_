36a88502b7eb0f5fa1359e16f6879f65cf39d563
XomsEdu <144520337+XomsEdu@users.noreply.github.com>
2025-02-14 03:17:55 +0200

    Fix try2

--- Assets/Code/Managers/Inventory/InventoryManager.cs
+++ Assets/Code/Managers/Inventory/InventoryManager.cs
@@ -9,41 +9,41 @@ public class InventoryManager : MonoBehaviour
     public GameObject inventoryItemPrefab;
     
     public bool AddItem(Item item)
+    {
+        for (int i = 0; i < inventorySlots.Length; i++)
         {
-            for (int i = 0; i < inventorySlots.Length; i++) //for stacks
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+
+            if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < itemInSlot.maxStackSize)
             {
-                InventorySlot slot = inventorySlots[i];
-                InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-
-                if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < itemInSlot.maxStackSize)
-                {
-                    itemInSlot.count++;
-                    itemInSlot.RefreshCount();
-                    return true;
-                }
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
             }
-
-            for (int i = 0; i < inventorySlots.Length; i++) //for new items
-            {
-                InventorySlot slot = inventorySlots[i];
-                InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-
-                if (itemInSlot == null)
-                {
-                    SpawnNewItem(item, slot);
-                    return true;
-                }
-            }
-
-            return false;
         }
+
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+
+            if (itemInSlot == null)
+            {
+                SpawnNewItem(item, slot);
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 
     void SpawnNewItem(Item item, InventorySlot slot)
-        {
-            GameObject newItemGO = Instantiate(inventoryItemPrefab, slot.transform);
-            InventoryItem inventoryItem = newItemGO.GetComponent<InventoryItem>();
-            inventoryItem.InitialiseItem(item);
-        }
+    {
+        GameObject newItemGO = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGO.GetComponent<InventoryItem>();
+        inventoryItem.InitialiseItem(item);
+    }
 
 }
