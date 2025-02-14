edeb917f9c8110958f3fb4ef429d9f51ce20f330
XomsEdu <144520337+XomsEdu@users.noreply.github.com>
2025-02-14 05:34:26 +0200

    Purge
    Stumbled on serious performance issues, needed to find solutions, ruined a few commits

--- Assets/Code/Managers/Inventory/InventoryManager.cs
+++ Assets/Code/Managers/Inventory/InventoryManager.cs
@@ -9,41 +9,41 @@ public class InventoryManager : MonoBehaviour
     public GameObject inventoryItemPrefab;
     
     public bool AddItem(Item item)
-    {
-        for (int i = 0; i < inventorySlots.Length; i++)
         {
-            InventorySlot slot = inventorySlots[i];
-            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-
-            if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < itemInSlot.maxStackSize)
+            for (int i = 0; i < inventorySlots.Length; i++) //for stacks
             {
-                itemInSlot.count++;
-                itemInSlot.RefreshCount();
-                return true;
+                InventorySlot slot = inventorySlots[i];
+                InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+
+                if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < itemInSlot.maxStackSize)
+                {
+                    itemInSlot.count++;
+                    itemInSlot.RefreshCount();
+                    return true;
+                }
             }
-        }
 
-        for (int i = 0; i < inventorySlots.Length; i++)
-        {
-            InventorySlot slot = inventorySlots[i];
-            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-
-            if (itemInSlot == null)
+            for (int i = 0; i < inventorySlots.Length; i++) //for new items
             {
-                SpawnNewItem(item, slot);
-                return true;
-            }
-        }
+                InventorySlot slot = inventorySlots[i];
+                InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
 
-        return false;
-    }
+                if (itemInSlot == null)
+                {
+                    SpawnNewItem(item, slot);
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
 
     void SpawnNewItem(Item item, InventorySlot slot)
-    {
-        GameObject newItemGO = Instantiate(inventoryItemPrefab, slot.transform);
-        InventoryItem inventoryItem = newItemGO.GetComponent<InventoryItem>();
-        inventoryItem.InitialiseItem(item);
-    }
+        {
+            GameObject newItemGO = Instantiate(inventoryItemPrefab, slot.transform);
+            InventoryItem inventoryItem = newItemGO.GetComponent<InventoryItem>();
+            inventoryItem.InitialiseItem(item);
+        }
 
 }
---
+++ Assets/Code/TransferableItems/PlayerManager.cs
@@ -0,0 +1,158 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [Header ("External components")]
+    InputManager inputManager;
+    Rigidbody pRigidbody;
+    Transform cameraObject;
+    
+
+    [Header ("Internal values")]
+    private Vector3 moveDirection;
+    private int jumpCount;
+    private bool fallStateSwitch;
+    private float coyoteTimeCount;
+
+
+    [Header ("Movement modifiers values")]
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+    public int jumpAmount = 1;
+    public float fallMultiplier = 2.5f;
+    public float jumpResistMultiplier = 2f;
+    public float gRayDistance = 0.5f;
+    public float coyoteTime = 0.1f;
+
+
+    [Header ("Layer modifiers")]
+    public LayerMask groundMask;
+    //public LayerMask aimMask;
+
+
+    [Header ("Behaviour booleans")]
+    public bool isGrounded;
+
+    private void Awake()
+        {
+            inputManager = GetComponent<InputManager>();
+            pRigidbody = GetComponent<Rigidbody>();
+            cameraObject = Camera.main.transform;
+        }
+
+    private void Update()
+        {
+            //PlayerAim();
+            //PlayerInteraction();
+        }
+
+    private void FixedUpdate()
+        {
+            PlayerMovement();
+            PlayerRotation();
+            GroundCheck();
+            GravityUpdate();
+            PlayerJump();
+        }
+
+    private void PlayerMovement()
+        {
+            moveDirection = cameraObject.forward * inputManager.movementInput.y;
+            moveDirection = moveDirection + cameraObject.right * inputManager.movementInput.x;
+            moveDirection.Normalize();
+            moveDirection.y = 0;
+
+            pRigidbody.velocity = new Vector3(moveDirection.x * moveSpeed, pRigidbody.velocity.y, moveDirection.z * moveSpeed);
+
+          /*  if (moveDirection.x != 0f && !isFalling || moveDirection.z != 0f && !isFalling)
+                {
+                    isWalking = true;
+                }
+            else
+                {
+                    isWalking = false;
+                } */ //to transfer into animation manager
+        }
+
+    private void PlayerRotation()
+        {
+            Vector3 targetDirection = Vector3.zero;
+
+            targetDirection = cameraObject.forward * inputManager.movementInput.y;
+            targetDirection = targetDirection + cameraObject.right * inputManager.movementInput.x;
+            targetDirection.Normalize();
+            targetDirection.y = 0;
+
+            if (targetDirection == Vector3.zero)
+                targetDirection = transform.forward;
+
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+           pRigidbody.MoveRotation(playerRotation);
+        }
+
+    
+    private void GroundCheck()
+        {
+            Vector3 gRayOrigin = transform.position;
+            
+            RaycastHit hit;
+
+            isGrounded = Physics.Raycast(gRayOrigin, -Vector3.up, out hit, gRayDistance, groundMask);
+            
+            if (isGrounded)
+                {
+                    jumpCount = jumpAmount;
+                    coyoteTimeCount = coyoteTime;
+
+                    if (fallStateSwitch) 
+                        {
+                            fallStateSwitch = false;
+                            inputManager.isJump = Input.GetButton("Jump");
+                        }
+                    
+                }
+            else
+                {
+                    fallStateSwitch = true;
+                    coyoteTimeCount -= Time.deltaTime;
+                }
+        }
+
+
+    private void GravityUpdate()
+        {
+            if (pRigidbody.velocity.y < 0) 
+                {
+                    pRigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier) * Time.deltaTime;  //falling
+                }
+            else if (pRigidbody.velocity.y > 0 && !Input.GetButton ("Jump")) 
+                {
+                    pRigidbody.velocity += Vector3.up * Physics.gravity.y * (jumpResistMultiplier) * Time.deltaTime;  //low jump resistance force
+                }
+        }
+
+
+    private void PlayerJump()
+        {
+            if (inputManager.isJump)
+                {
+                   if (coyoteTimeCount > 0f)
+                        {
+                            pRigidbody.velocity = new Vector3(pRigidbody.velocity.x, jumpForce, pRigidbody.velocity.z);
+                            //isJumping = true;
+                        }
+
+                    else if (jumpCount > 0)
+                        {
+                            jumpCount -= 1;
+                            pRigidbody.velocity = new Vector3(pRigidbody.velocity.x, jumpForce, pRigidbody.velocity.z);
+                            //isJumping = true;
+                        }
+                    
+                    inputManager.isJump = false;
+                }
+        }
+}
