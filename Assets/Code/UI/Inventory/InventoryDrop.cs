e7ab4c1d023807f0635a201347c9b0beb75dcfb9
XomsEdu <144520337+XomsEdu@users.noreply.github.com>
2025-02-28 00:58:30 +0200

    Refactor of drop
    Ditched itemSpawnManager to avoid unwanted dependency in future (even though separation of concerns is a good practice, i don`t want to depend on monobehaviour item for a random method)

--- Assets/Code/UI/Inventory/InventoryDrop.cs
+++ Assets/Code/UI/Inventory/InventoryDrop.cs
@@ -3,29 +3,29 @@ using UnityEngine.EventSystems;
 
 public class InventoryDrop : MonoBehaviour, IDropHandler
 {
-    private ItemSpawnManager itemSpawnManager;
-
     public Transform dropPlace;
-
-    private void Awake()
-        {
-            itemSpawnManager = FindObjectOfType<ItemSpawnManager>();
-            if (itemSpawnManager == null)
-                Debug.LogError("ItemSpawnManager not found in the scene!");
-        }
+    public GameObject droppedItemPrefab;
 
     public void OnDrop(PointerEventData eventData)
-    {
-        GameObject dropped = eventData.pointerDrag;
-        if (dropped != null)
         {
-            InventoryItem dragableItem = dropped.GetComponent<InventoryItem>();
-            if (dragableItem != null && itemSpawnManager != null)
+            GameObject dropped = eventData.pointerDrag;
+            if (dropped != null)
             {
-                itemSpawnManager.CreateDroppedItem(dragableItem.item, dragableItem.count, dropPlace.position);
-                Destroy(dropped);
+                InventoryItem dragableItem = dropped.GetComponent<InventoryItem>();
+                if (dragableItem != null)
+                {
+                    CreateDroppedItem(dragableItem.item, dragableItem.count, dropPlace.position);
+                    Destroy(dropped);
+                }
             }
         }
-    }
+
+    private void CreateDroppedItem(Item item, int stackSize, Vector3 position)
+        {
+            GameObject droppedItemGO = Instantiate(droppedItemPrefab, position, Quaternion.identity);
+            DroppedItem droppedItemScript = droppedItemGO.GetComponentInChildren<DroppedItem>();
+
+            droppedItemScript.item = item;
+            droppedItemScript.stackSize = stackSize;
+        }
 }
-//sa
